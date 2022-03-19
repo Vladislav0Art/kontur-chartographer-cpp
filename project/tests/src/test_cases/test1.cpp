2f52274e7ad52bd4a6048bb9e8de0b083f09a6b4
Vladislav0Art <vladislav0art@gmail.com>
2022-03-20 00:55:59 +0300

    Update tests

---
+++ project/tests/src/test_cases/test1.cpp
@@ -0,0 +1,14 @@
+﻿// catch2
+#include "catch2/catch.hpp"
+// charta
+#include "charta/hello.h"
+#include "charta/canvas_manager.h"
+
+using namespace charta;
+
+
+
+TEST_CASE("Creating empty bmp image") {
+	CanvasManager::createNewCanvas(100, 120, "./test.bmp");
+	// CanvasManager::deleteCanvas("./test.bmp");
+}
