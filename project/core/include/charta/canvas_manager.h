4e4b5c48a80442ae8ad4c5ef7e78c24adb4a467d
Vladislav0Art <vladislav0art@gmail.com>
2022-03-20 15:19:27 +0300

    Add BMP meta headers and read() method

--- project/core/include/charta/canvas_manager.h
+++ project/core/include/charta/canvas_manager.h
@@ -1,20 +1,57 @@
 #pragma once
 #include <string>
 #include <filesystem>
+#include <fstream>
 
 namespace charta {
 	
+#pragma pack(push, 1)
+
+struct BMPFileHeader {
+	std::uint16_t file_type = 0;
+	std::uint32_t file_size = 0;
+	std::uint16_t reserved1 = 0;
+	std::uint16_t reserved2 = 0;
+	std::uint32_t offset_data = 0;
+
+	inline static const std::uint16_t REQUIRED_FILE_TYPE = 0x4D42;
+};
+
+struct BMPInfoHeader {
+	std::uint32_t size = 0;
+	std::int32_t width = 0;
+	std::int32_t height = 0;
+	std::uint16_t planes = 1;
+	std::uint16_t bit_count = 0;
+	std::uint32_t compression = 0;
+	std::uint32_t size_image = 0;
+	std::int32_t xpixels_per_meter = 0;
+	std::int32_t ypixels_per_meter = 0;
+	std::uint32_t colors_used = 0;
+	std::uint32_t colors_important = 0;
+
+	// inline static const std::uint16_t REQUIRED_BIT_COUNT = 24;
+};
+
+#pragma pack(pop)
+
 struct CanvasManager {
 	// 1. create image and return its uuid
 	// 2. delete image by uuid
 	// 3. save restored fragment of image
 	// 4. return restore fragment of image
 
+
 	CanvasManager(std::filesystem::path working_folder_) 
 		: m_working_folder(working_folder_.string() + "/") {}
 
 	void createNewCanvas(int width, int height, const std::string& filepath);
 	void deleteCanvas(const std::string& filepath);
+
+	void read(std::istream &stream);
+
+	BMPFileHeader file_header;
+    BMPInfoHeader bmp_info_header;
 
 private:
 	const std::filesystem::path m_working_folder;
--- project/core/src/canvas_manager.cpp
+++ project/core/src/canvas_manager.cpp
@@ -1,6 +1,9 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
 // charta
 #include "charta/canvas_manager.h"
 #include "charta/exceptions.h"
@@ -27,6 +30,7 @@ void CanvasManager::createNewCanvas(const int width, const int height, const std
 
 	std::string filepath = m_working_folder.string() + filename;
 	std::cout << filepath << std::endl;
+	// TODO: if m_working_folder does not exist stb will not create bmp file
 	const int status = stbi_write_bmp(filepath.c_str(), width, height, CanvasManager::Channels, canvas.data());
 
 	if(!status) {
@@ -43,6 +47,47 @@ void CanvasManager::deleteCanvas(const std::string& filename) {
 	if(remove(filepath.c_str()) != 0) {
 		throw FileDeletionFailure();
 	}
+}
+
+
+void CanvasManager::read(std::istream &stream) {
+	// file header
+	std::stringstream binary_sstream(std::stringstream::in | std::stringstream::out | std::stringstream::binary);
+	std::uint8_t byte;
+	while(stream >> byte) {
+		binary_sstream << byte;
+	}
+
+    binary_sstream.read(reinterpret_cast<char*>(&file_header), sizeof(file_header));
+
+    if (file_header.file_type != BMPFileHeader::REQUIRED_FILE_TYPE) {
+        std::cout << "Wrong file type: " << file_header.file_type << std::endl;
+    }
+
+    // info header
+    binary_sstream.read(reinterpret_cast<char *>(&bmp_info_header), sizeof(bmp_info_header));
+
+    if (bmp_info_header.size != sizeof(BMPInfoHeader)) {
+		std::cout << "Invalid version! provided: " << bmp_info_header.size 
+				  << "; required: " << sizeof(BMPInfoHeader) <<std::endl;
+    }
+
+    // find pixel data location
+    binary_sstream.seekg(file_header.offset_data, std::ifstream::beg);
+
+    // // bytes count of each row without padding
+    const auto row_in_bytes = bmp_info_header.width * bmp_info_header.bit_count / 8U;
+    const auto padding = (4 - row_in_bytes % 4) % 4;
+
+    std::vector<std::uint8_t> data((row_in_bytes + padding) * bmp_info_header.height);
+	binary_sstream.read(reinterpret_cast<char*>(data.data()), static_cast<std::streamsize>(data.size()));
+
+	// writing
+    std::ofstream file("./test.bmp", std::ofstream::out | std::ofstream::binary);
+
+    file.write(reinterpret_cast<char*>(&file_header), sizeof(file_header));
+    file.write(reinterpret_cast<char*>(&bmp_info_header), sizeof(bmp_info_header));
+    file.write(reinterpret_cast<char *>(data.data()), static_cast<std::streamsize>(data.size()));
 }
 
 
